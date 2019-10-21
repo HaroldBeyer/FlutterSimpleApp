07d2929262945df4b4af3f71477272497b9a8d04
Haroldo Beyer <haroldobeyerbacellar@gmail.com>
2019-10-21 19:39:01 -0300

    Remoção

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,12 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,15 +68,25 @@ class _HomePageState extends State<HomePage> {
           itemCount: widget.items.length,
           itemBuilder: (BuildContext ctxt, int index) {
             final item = widget.items[index];
-            return CheckboxListTile(
-              title: Text(item.title),
+            return Dismissible(
               key: Key(item.title),
-              value: item.done,
-              onChanged: (value) {
-                setState(() {
-                  item.done = value;
-                });
+              background: Container(
+                color: Colors.red.withOpacity(0.9),
+                child: Text("Excluir"),
+              ),
+              onDismissed: (direction) {
+                print(direction);
+                remove(index);
               },
+              child: CheckboxListTile(
+                title: Text(item.title),
+                value: item.done,
+                onChanged: (value) {
+                  setState(() {
+                    item.done = value;
+                  });
+                },
+              ),
             );
           },
         ),
